f1349122e97e2e7b10f01196c1ea3f5a71cc34d1
Paul Thompson <pmt10@case.edu>
2024-04-02 21:44:04 -0400

    make protected members private in timeframe

--- src/DataManager/TimeFrame.h
+++ src/DataManager/TimeFrame.h
@@ -4,24 +4,26 @@
 
 class TimeFrame {
 public:
-    void updateTotalFrameCount(int frame_count) {this->total_frame_count = frame_count;};
-    int getTotalFrameCount() const {return this->total_frame_count;};
+    void updateTotalFrameCount(int frame_count) {_total_frame_count = frame_count;};
+    int getTotalFrameCount() const {return _total_frame_count;};
 
-    void updateLastLoadedFrame(int frame) {this->last_loaded_frame = frame;};
-    int getLastLoadedFrame() const {return this->last_loaded_frame;};
+    void updateLastLoadedFrame(int frame) {_last_loaded_frame = frame;};
+    int getLastLoadedFrame() const {return _last_loaded_frame;};
 
     int checkFrameInbounds(int frame_id) {
 
         if (frame_id < 0) {
             frame_id = 0;
-        } else if (frame_id >= this->total_frame_count - 1) {
-            frame_id = this->total_frame_count - 1;
+        } else if (frame_id >= _total_frame_count - 1) {
+            frame_id = _total_frame_count - 1;
         }
         return frame_id;
     }
 protected:
-    int last_loaded_frame;
-    int total_frame_count;
+
+private:
+    int _last_loaded_frame;
+    int _total_frame_count;
 };
 
 
