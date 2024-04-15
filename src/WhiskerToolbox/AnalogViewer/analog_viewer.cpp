1f60d20f80e53207091504084e0558538600906f
Paul Thompson <pmt10@case.edu>
2024-04-15 13:29:21 -0400

    define analog_viewer class

--- src/WhiskerToolbox/AnalogViewer/analog_viewer.cpp
+++ src/WhiskerToolbox/AnalogViewer/analog_viewer.cpp
@@ -4,3 +4,18 @@
 
 #include "analog_viewer.hpp"
 
+#include "ui_analog_viewer.h"
+
+Analog_Viewer::Analog_Viewer(Media_Window* scene, std::shared_ptr<TimeFrame> time, QWidget *parent) :
+    QWidget(parent),  ui(new Ui::Analog_Viewer)
+{
+    ui->setupUi(this);
+
+    _scene = scene;
+    _time = time;
+
+}
+
+Analog_Viewer::~Analog_Viewer() {
+    delete ui;
+}
--- src/WhiskerToolbox/AnalogViewer/analog_viewer.hpp
+++ src/WhiskerToolbox/AnalogViewer/analog_viewer.hpp
@@ -5,4 +5,38 @@
 #ifndef WHISKERTOOLBOX_ANALOG_VIEWER_HPP
 #define WHISKERTOOLBOX_ANALOG_VIEWER_HPP
 
+#include <QWidget>
+
+#include "Media_Window.h"
+#include "TimeFrame.h"
+
+#include <memory>
+
+namespace Ui {
+class Analog_Viewer;
+}
+
+class Analog_Viewer : public QWidget {
+    Q_OBJECT
+public:
+    Analog_Viewer(Media_Window *scene, std::shared_ptr<TimeFrame> time, QWidget *parent = 0);
+    ~Analog_Viewer();
+
+    //void openWidget(); // Call
+
+protected:
+    //void closeEvent(QCloseEvent *event);
+    //void keyPressEvent(QKeyEvent *event);
+
+private:
+    Media_Window *_scene;
+    std::shared_ptr<TimeFrame> _time;
+
+    Ui::Analog_Viewer *ui;
+private slots:
+
+};
+
+
+
 #endif //WHISKERTOOLBOX_ANALOG_VIEWER_HPP
