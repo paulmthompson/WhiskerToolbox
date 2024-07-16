4e5bed465e695a5cb06d84649e28d81ea10e479a
Paul Thompson <pmt10@case.edu>
2024-07-15 21:57:13 -0400

    don't fatal error if codesigning doesn't work

--- packaging/Install_Apple.cmake
+++ packaging/Install_Apple.cmake
@@ -84,8 +84,5 @@ install(CODE "
         OUTPUT_VARIABLE output
         ERROR_VARIABLE error
     )
-    if(NOT \${result} EQUAL 0)
-        message(FATAL_ERROR \"Failed to codesign WhiskerToolbox.app: \${error}\")
-    endif()
-    message(STATUS \"Successfully codesigned WhiskerToolbox.app\")
+
 ")
