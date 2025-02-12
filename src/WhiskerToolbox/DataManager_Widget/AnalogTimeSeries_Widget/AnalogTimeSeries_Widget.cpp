835bd3c0ae2fc0b1e512a148a8e64d58a04b806b
Paul Thompson <pmt10@case.edu>
2025-02-12 13:48:40 -0500

    can save analog time series from datamanaget_widget

--- src/WhiskerToolbox/DataManager_Widget/AnalogTimeSeries_Widget/AnalogTimeSeries_Widget.cpp
+++ src/WhiskerToolbox/DataManager_Widget/AnalogTimeSeries_Widget/AnalogTimeSeries_Widget.cpp
@@ -1,6 +1,11 @@
 #include "AnalogTimeSeries_Widget.hpp"
 #include "ui_AnalogTimeSeries_Widget.h"
 
+#include "DataManager/AnalogTimeSeries/Analog_Time_Series.hpp"
+#include "DataManager/DataManager.hpp"
+
+#include <iostream>
+
 AnalogTimeSeries_Widget::AnalogTimeSeries_Widget(std::shared_ptr<DataManager> data_manager, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AnalogTimeSeries_Widget),
@@ -8,7 +13,7 @@ AnalogTimeSeries_Widget::AnalogTimeSeries_Widget(std::shared_ptr<DataManager> da
 {
     ui->setupUi(this);
 
-    // Connect signals and slots here
+    connect(ui->save_csv, &QPushButton::clicked, this, &AnalogTimeSeries_Widget::_saveCSV);
 }
 
 AnalogTimeSeries_Widget::~AnalogTimeSeries_Widget() {
@@ -20,3 +25,31 @@ void AnalogTimeSeries_Widget::openWidget()
     // Populate the widget with data if needed
     this->show();
 }
+
+void AnalogTimeSeries_Widget::_saveCSV()
+{
+    auto output_path = _data_manager->getOutputPath();
+    std::cout << output_path.string() << std::endl;
+    std::cout << _active_key << std::endl;
+
+    auto filename = ui->filename_textbox->toPlainText().toStdString();
+    output_path.append(filename);
+
+    auto analog_data = _data_manager->getData<AnalogTimeSeries>(_active_key)->getAnalogTimeSeries();
+    auto time_stamps = _data_manager->getData<AnalogTimeSeries>(_active_key)->getTimeSeries();
+
+    save_analog(analog_data, time_stamps, output_path.string());
+}
+
+
+void AnalogTimeSeries_Widget::setActiveKey(std::string key)
+{
+    _active_key = key;
+
+    //_data_manager->removeCallbackFromData(key, _callback_id);
+
+    //_assignCallbacks();
+
+    //_calculateIntervals();
+}
+
