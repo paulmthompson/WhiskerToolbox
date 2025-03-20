50e52f24167b0a931f4a4b969a3f18db7c3f3d3a
Paul Thompson <pmt10@case.edu>
2025-03-20 13:09:51 -0400

    test if clang-tidy works with changed file now

--- src/WhiskerToolbox/DataManager/DigitalTimeSeries/Digital_Event_Series.cpp
+++ src/WhiskerToolbox/DataManager/DigitalTimeSeries/Digital_Event_Series.cpp
@@ -3,16 +3,17 @@
 #include <fstream>
 #include <sstream>
 
-DigitalEventSeries::DigitalEventSeries(std::vector<float> event_vector) : _data(std::move(event_vector)) {}
+DigitalEventSeries::DigitalEventSeries(std::vector<float> event_vector)
+    : _data(std::move(event_vector)) {}
 
-std::vector<float> const& DigitalEventSeries::getEventSeries() const {
+std::vector<float> const & DigitalEventSeries::getEventSeries() const {
     return _data;
 }
 
 namespace DigitalEventSeriesLoader {
 
-DigitalEventSeries loadFromCSV(const std::string& filename) {
+DigitalEventSeries loadFromCSV(std::string const & filename) {
     return DigitalEventSeries(CSVLoader::loadSingleColumnCSV(filename));
 }
 
-} // namespace DigitalEventSeriesLoader
+}// namespace DigitalEventSeriesLoader
--- src/WhiskerToolbox/DataManager/DigitalTimeSeries/Digital_Event_Series.hpp
+++ src/WhiskerToolbox/DataManager/DigitalTimeSeries/Digital_Event_Series.hpp
@@ -22,15 +22,15 @@ public:
     DigitalEventSeries(std::vector<float> event_vector);
 
     void setData(std::vector<float> event_vector) { _data = event_vector; }
-    std::vector<float> const& getEventSeries() const;
+    std::vector<float> const & getEventSeries() const;
 
 private:
-    std::vector<float> _data {};
+    std::vector<float> _data{};
 };
 
 namespace DigitalEventSeriesLoader {
-    DigitalEventSeries loadFromCSV(const std::string& filename);
-} // namespace DigitalEventSeriesLoader
+DigitalEventSeries loadFromCSV(std::string const & filename);
+}// namespace DigitalEventSeriesLoader
 
 
-#endif //BEHAVIORTOOLBOX_DIGITAL_EVENT_SERIES_HPP
+#endif//BEHAVIORTOOLBOX_DIGITAL_EVENT_SERIES_HPP
