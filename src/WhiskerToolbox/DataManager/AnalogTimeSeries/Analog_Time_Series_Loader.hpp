313f909560264284df9ca67932645c2050938003
Paul Thompson <pmt10@case.edu>
2025-04-15 10:56:53 -0400

    analog time series loader and digital interval series loader cpp files created

--- src/WhiskerToolbox/DataManager/AnalogTimeSeries/Analog_Time_Series_Loader.hpp
+++ src/WhiskerToolbox/DataManager/AnalogTimeSeries/Analog_Time_Series_Loader.hpp
@@ -1,10 +1,6 @@
 #ifndef ANALOG_TIME_SERIES_LOADER_HPP
 #define ANALOG_TIME_SERIES_LOADER_HPP
 
-#include "AnalogTimeSeries/Analog_Time_Series.hpp"
-#include "loaders/binary_loaders.hpp"
-#include "utils/json_helpers.hpp"
-
 #include "nlohmann/json.hpp"
 
 #include <memory>
@@ -16,77 +12,12 @@ enum class AnalogDataType {
     Unknown
 };
 
-AnalogDataType stringToAnalogDataType(std::string const & data_type_str) {
-    if (data_type_str == "int16") return AnalogDataType::int16;
-    return AnalogDataType::Unknown;
-}
+class AnalogTimeSeries;
 
+AnalogDataType stringToAnalogDataType(std::string const & data_type_str);
 
-inline std::vector<std::shared_ptr<AnalogTimeSeries>> load_into_AnalogTimeSeries(std::string const & file_path, nlohmann::basic_json<> const & item) {
-
-    std::vector<std::shared_ptr<AnalogTimeSeries>> analog_time_series;
-
-    if (!requiredFieldsExist(
-                item,
-                {"format"},
-                "Error: Missing required fields in AnalogTimeSeries")) {
-        return analog_time_series;
-    }
-    std::string const data_type_str = item["format"];
-    AnalogDataType const data_type = stringToAnalogDataType(data_type_str);
-
-    switch (data_type) {
-        case AnalogDataType::int16: {
-
-            int const header_size = item.value("header_size", 0);
-            int const num_channels = item.value("channel_count", 1);
-
-            auto opts = Loader::BinaryAnalogOptions{
-                    .file_path = file_path,
-                    .header_size_bytes = static_cast<size_t>(header_size),
-                    .num_channels = static_cast<size_t>(num_channels)};
-
-            if (opts.num_channels > 1) {
-
-                auto data = readBinaryFileMultiChannel<int16_t>(opts);
-
-                std::cout << "Read " << data.size() << " channels" << std::endl;
-
-                for (auto & channel: data) {
-                    // convert to float with std::transform
-                    std::vector<float> data_float;
-                    std::transform(
-                            channel.begin(),
-                            channel.end(),
-                            std::back_inserter(data_float), [](int16_t i) { return i; });
-
-                    analog_time_series.push_back(std::make_shared<AnalogTimeSeries>());
-                    analog_time_series.back()->setData(data_float);
-                }
-
-            } else {
-
-                auto data = readBinaryFile<int16_t>(opts);
-
-                // convert to float with std::transform
-                std::vector<float> data_float;
-                std::transform(
-                        data.begin(),
-                        data.end(),
-                        std::back_inserter(data_float), [](int16_t i) { return i; });
-
-                analog_time_series.push_back(std::make_shared<AnalogTimeSeries>());
-                analog_time_series.back()->setData(data_float);
-            }
-            break;
-        }
-        default: {
-            std::cout << "Format " << data_type_str << " not found " << std::endl;
-        }
-    }
-
-    return analog_time_series;
-}
+std::vector<std::shared_ptr<AnalogTimeSeries>> load_into_AnalogTimeSeries(std::string const & file_path,
+                                                                          nlohmann::basic_json<> const & item);
 
 
 #endif// ANALOG_TIME_SERIES_LOADER_HPP
---
+++ src/WhiskerToolbox/DataManager/DigitalTimeSeries/Digital_Interval_Series_Loader.cpp
@@ -0,0 +1,74 @@
+
+#include "Digital_Interval_Series_Loader.hpp"
+
+#include "DigitalTimeSeries/Digital_Interval_Series.hpp"
+#include "loaders/CSV_Loaders.hpp"
+#include "loaders/binary_loaders.hpp"
+#include "utils/json_helpers.hpp"
+
+
+IntervalDataType stringToIntervalDataType(std::string const & data_type_str) {
+    if (data_type_str == "uint16") return IntervalDataType::uint16;
+    if (data_type_str == "csv") return IntervalDataType::csv;
+    return IntervalDataType::Unknown;
+}
+
+std::shared_ptr<DigitalIntervalSeries> load_into_DigitalIntervalSeries(std::string const & file_path,
+                                                                       nlohmann::basic_json<> const & item) {
+    auto digital_interval_series = std::make_shared<DigitalIntervalSeries>();
+
+    if (!requiredFieldsExist(
+                item,
+                {"format"},
+                "Error: Missing required fields in DigitalIntervalSeries")) {
+        return digital_interval_series;
+    }
+    std::string const data_type_str = item["format"];
+    IntervalDataType const data_type = stringToIntervalDataType(data_type_str);
+
+    switch (data_type) {
+        case IntervalDataType::uint16: {
+
+            if (!requiredFieldsExist(
+                        item,
+                        {"channel", "transition"},
+                        "Error: Missing required fields in uint16 DigitalIntervalSeries")) {
+                return digital_interval_series;
+            }
+
+            int const channel = item["channel"];
+            std::string const transition = item["transition"];
+
+            int const header_size = item.value("header_size", 0);
+            int const num_channels = item.value("channel_count", 1);
+
+            auto opts = Loader::BinaryAnalogOptions{
+                                                    .file_path = file_path,
+                                                    .header_size_bytes = static_cast<size_t>(header_size),
+                                                    .num_channels = static_cast<size_t>(num_channels)};
+            auto data = readBinaryFile<uint16_t>(opts);
+
+            auto digital_data = Loader::extractDigitalData(data, channel);
+
+            auto intervals = Loader::extractIntervals(digital_data, transition);
+            std::cout << "Loaded " << intervals.size() << " intervals " << std::endl;
+
+            digital_interval_series->setData(intervals);
+            break;
+        }
+        case IntervalDataType::csv: {
+
+            auto opts = Loader::CSVPairColumnOptions{.filename = file_path};
+
+            auto intervals = Loader::loadPairColumnCSV(opts);
+            std::cout << "Loaded " << intervals.size() << " intervals " << std::endl;
+            digital_interval_series->setData(intervals);
+            break;
+        }
+        default: {
+            std::cout << "Format " << data_type_str << " not found " << std::endl;
+        }
+    }
+
+    return digital_interval_series;
+}
