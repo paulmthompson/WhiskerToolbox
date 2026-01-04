c7551c859902c79c191847a694ce1389b5ab2619
Paul Thompson <pmt10@case.edu>
2026-01-04 15:32:30 -0500

    moving more inline functions into cpp in coreplotting

--- src/CorePlotting/CoordinateTransform/TimeAxisCoordinates.hpp
+++ src/CorePlotting/CoordinateTransform/TimeAxisCoordinates.hpp
@@ -160,17 +160,7 @@ struct YAxisParams {
  * float ndc_right = timeToNDC(200.0f, params); // Returns +1.0f
  * @endcode
  */
-[[nodiscard]] inline float timeToNDC(float time, TimeAxisParams const & params) {
-    float const time_span = static_cast<float>(params.time_end - params.time_start);
-
-    if (time_span <= 0.0f) {
-        return 0.0f;
-    }
-
-    // Map [time_start, time_end] to [-1, +1]
-    float const normalized = (time - static_cast<float>(params.time_start)) / time_span;
-    return 2.0f * normalized - 1.0f;
-}
+[[nodiscard]] float timeToNDC(float time, TimeAxisParams const & params);
 
 /**
  * @brief Convert NDC X coordinate to time coordinate
@@ -187,13 +177,7 @@ struct YAxisParams {
  * float time = ndcToTime(0.0f, params); // Returns 150.0f (center)
  * @endcode
  */
-[[nodiscard]] inline float ndcToTime(float ndc_x, TimeAxisParams const & params) {
-    // Map [-1, +1] to [time_start, time_end]
-    float const normalized = (ndc_x + 1.0f) / 2.0f;
-    float const time_span = static_cast<float>(params.time_end - params.time_start);
-
-    return static_cast<float>(params.time_start) + normalized * time_span;
-}
+[[nodiscard]] float ndcToTime(float ndc_x, TimeAxisParams const & params);
 
 /**
  * @brief Calculate pixels per time unit
@@ -204,15 +188,7 @@ struct YAxisParams {
  * @param params Time axis parameters
  * @return Pixels per time unit (can be fractional)
  */
-[[nodiscard]] inline float pixelsPerTimeUnit(TimeAxisParams const & params) {
-    float const time_span = static_cast<float>(params.time_end - params.time_start);
-
-    if (time_span <= 0.0f) {
-        return 0.0f;
-    }
-
-    return static_cast<float>(params.viewport_width_px) / time_span;
-}
+[[nodiscard]] float pixelsPerTimeUnit(TimeAxisParams const & params);
 
 /**
  * @brief Calculate time units per pixel
@@ -223,14 +199,7 @@ struct YAxisParams {
  * @param params Time axis parameters
  * @return Time units per pixel
  */
-[[nodiscard]] inline float timeUnitsPerPixel(TimeAxisParams const & params) {
-    if (params.viewport_width_px <= 0) {
-        return 0.0f;
-    }
-
-    float const time_span = static_cast<float>(params.time_end - params.time_start);
-    return time_span / static_cast<float>(params.viewport_width_px);
-}
+[[nodiscard]] float timeUnitsPerPixel(TimeAxisParams const & params);
 
 /**
  * @brief Convenience method to create TimeAxisParams from TimeSeriesViewState
@@ -266,23 +235,7 @@ struct YAxisParams {
  * float world_y_bot = canvasYToWorldY(600.0f, params); // Returns -1.0f (bottom)
  * @endcode
  */
-[[nodiscard]] inline float canvasYToWorldY(float canvas_y, YAxisParams const & params) {
-    if (params.viewport_height_px <= 0) {
-        return params.world_y_min + params.pan_offset;
-    }
-
-    // Canvas Y: 0 = top, viewport_height = bottom
-    // World Y: y_max = top, y_min = bottom
-    // So we need to invert: normalized_y = 1 - (canvas_y / height)
-    float const normalized_y = 1.0f - (canvas_y / static_cast<float>(params.viewport_height_px));
-
-    // Apply pan offset to effective bounds
-    float const effective_y_min = params.world_y_min + params.pan_offset;
-    float const effective_y_max = params.world_y_max + params.pan_offset;
-
-    // Map [0, 1] to [effective_y_min, effective_y_max]
-    return effective_y_min + normalized_y * (effective_y_max - effective_y_min);
-}
+[[nodiscard]] float canvasYToWorldY(float canvas_y, YAxisParams const & params);
 
 /**
  * @brief Convert world Y coordinate to canvas Y pixel coordinate
@@ -293,22 +246,7 @@ struct YAxisParams {
  * @param params Y axis parameters
  * @return Canvas Y position in pixels (0 = top of canvas)
  */
-[[nodiscard]] inline float worldYToCanvasY(float world_y, YAxisParams const & params) {
-    // Apply pan offset to effective bounds
-    float const effective_y_min = params.world_y_min + params.pan_offset;
-    float const effective_y_max = params.world_y_max + params.pan_offset;
-    float const y_range = effective_y_max - effective_y_min;
-
-    if (y_range <= 0.0f || params.viewport_height_px <= 0) {
-        return 0.0f;
-    }
-
-    // Map world_y to normalized [0, 1] where 0 = bottom, 1 = top
-    float const normalized_y = (world_y - effective_y_min) / y_range;
-
-    // Invert for canvas coordinates (0 = top, height = bottom)
-    return (1.0f - normalized_y) * static_cast<float>(params.viewport_height_px);
-}
+[[nodiscard]] float worldYToCanvasY(float world_y, YAxisParams const & params);
 
 /**
  * @brief Convert world Y to Normalized Device Coordinate (NDC)
@@ -319,19 +257,7 @@ struct YAxisParams {
  * @param params Y axis parameters (pan offset is applied)
  * @return NDC Y coordinate in range [-1, +1]
  */
-[[nodiscard]] inline float worldYToNDC(float world_y, YAxisParams const & params) {
-    float const effective_y_min = params.world_y_min + params.pan_offset;
-    float const effective_y_max = params.world_y_max + params.pan_offset;
-    float const y_range = effective_y_max - effective_y_min;
-
-    if (y_range <= 0.0f) {
-        return 0.0f;
-    }
-
-    // Map [effective_y_min, effective_y_max] to [-1, +1]
-    float const normalized = (world_y - effective_y_min) / y_range;
-    return 2.0f * normalized - 1.0f;
-}
+[[nodiscard]] float worldYToNDC(float world_y, YAxisParams const & params);
 
 /**
  * @brief Convert NDC Y coordinate to world Y coordinate
@@ -342,14 +268,7 @@ struct YAxisParams {
  * @param params Y axis parameters
  * @return World Y coordinate
  */
-[[nodiscard]] inline float ndcToWorldY(float ndc_y, YAxisParams const & params) {
-    float const effective_y_min = params.world_y_min + params.pan_offset;
-    float const effective_y_max = params.world_y_max + params.pan_offset;
-
-    // Map [-1, +1] to [effective_y_min, effective_y_max]
-    float const normalized = (ndc_y + 1.0f) / 2.0f;
-    return effective_y_min + normalized * (effective_y_max - effective_y_min);
-}
+[[nodiscard]] float ndcToWorldY(float ndc_y, YAxisParams const & params);
 
 }// namespace CorePlotting
 
