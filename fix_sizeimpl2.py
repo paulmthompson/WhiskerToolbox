e77ea4afc8c7480d8cad9620356f3bf26544d6ca
Paul Thompson <pmt10@case.edu>
2026-03-09 15:00:05 -0400

    Delete fix_sizeimpl2.py

--- fix_sizeimpl2.py
+++
@@ -1,8 +0,0 @@
-with open('src/DataManager/DigitalTimeSeries/storage/OwningDigitalEventStorage.hpp', 'r') as f:
-    content = f.read()
-
-if 'sizeImpl() const' not in content:
-    content = content.replace('    void setEntityIds(std::vector<EntityId> ids);', '    void setEntityIds(std::vector<EntityId> ids);\n\n    // ========== CRTP Implementation ==========\n    [[nodiscard]] size_t sizeImpl() const { return _events.size(); }')
-
-with open('src/DataManager/DigitalTimeSeries/storage/OwningDigitalEventStorage.hpp', 'w') as f:
-    f.write(content)
